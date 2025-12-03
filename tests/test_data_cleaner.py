1ff863dcd798c1c7740863fd1dd4a355ff9d0768
pjorquierap275 <pjorquierapendola@gmail.com>
2025-12-03 15:42:31 -0300

    Update test_drop_invalid_rows_raises_keyerror_for_unknown_column
    respuesta 2 de A2

--- tests/test_data_cleaner.py
+++ tests/test_data_cleaner.py
@@ -101,11 +101,11 @@ class TestDataCleaner(unittest.TestCase):
         - Llamar a drop_invalid_rows con una columna que no existe (ej: "does_not_exist")
         - Verificar que se lanza un KeyError (usar self.assertRaises)
         """
-        #df = make_sample_df()
-        #cleaner = DataCleaner()
+        df = make_sample_df()
+        cleaner = DataCleaner()
     
-        #with self.assertRaises(KeyError):
-        #    cleaner.drop_invalid_rows(df, ["doesn't_exist"])
+        with self.assertRaises(KeyError):
+            cleaner.drop_invalid_rows(df, ["doesn't_exist"])
 
     def test_trim_strings_strips_whitespace_without_changing_other_columns(self):
         """Test que verifica que el método trim_strings elimina correctamente los espacios
